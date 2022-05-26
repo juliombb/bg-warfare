152c9b90416163f08cfda7f7b1a7159807f7802b
juliombb <juliogx@hotmail.com>
2022-05-26 00:43:29 -0300

    prevent useless client sends

--- Assets/Scripts/Client/ClientPositionMonitor.cs
+++ Assets/Scripts/Client/ClientPositionMonitor.cs
@@ -13,9 +13,10 @@ namespace Client
         private NetPeer _server;
         private Transform _monitoredTransform;
         private Vector3 _lastPosition = Vector3.zero;
-        private Quaternion _lastRotation = Quaternion.identity;
+        private Vector3 _lastRotation = Vector3.zero;
         private NetDataWriter _writer = new NetDataWriter();
-        private int sequence = 0;
+        private int _sequence = 0;
+        private float _epsilon = 0.01f;
         public void Setup(GameObject baseClient, ServerData server)
         {
             _server = server.Peer;
@@ -26,17 +27,19 @@ namespace Client
             if (Time.time - _lastUpdate < Config.TickRate) return;
             _writer.Reset();
             var position = _monitoredTransform.position;
-            var rotation = _monitoredTransform.rotation;
-            if (Vector3.Distance(position, _lastPosition) < Vector3.kEpsilon && _lastRotation == rotation)
+            var rotation = _monitoredTransform.rotation.eulerAngles;
+            if (Vector3.Distance(position, _lastPosition) < _epsilon && Vector3.Distance(rotation, _lastRotation) < _epsilon)
             {
                 return;
             }
+            _lastPosition = position;
+            _lastRotation = rotation;
             _writer.Put((byte)ServerCommand.PositionOfPlayer);
-            _writer.Put(BitConverter.GetBytes(sequence++), 0 , 4);
+            _writer.Put(BitConverter.GetBytes(_sequence++), 0 , 4);
             _writer.Put(BitConverter.GetBytes(position.x), 0 , 4);
             _writer.Put(BitConverter.GetBytes(position.y), 0 , 4);
             _writer.Put(BitConverter.GetBytes(position.z), 0 , 4);
-            _writer.Put(BitConverter.GetBytes(rotation.eulerAngles.y), 0 , 4);
+            _writer.Put(BitConverter.GetBytes(rotation.y), 0 , 4);
             _server.Send(_writer, DeliveryMethod.Unreliable);
             _lastUpdate = Time.time;
         }
